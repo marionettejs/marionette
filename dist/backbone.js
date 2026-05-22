a5e57519115aa8b1cc11ac4a4e869211ca3c77d0
Paul Falgout <paul.falgout@roundingwell.com>
2026-05-23 01:13:40 +0900

    fix(backbone): stop shim namespace pollution

--- dist/backbone.js
+++ dist/backbone.js
@@ -3,9 +3,7 @@ import Backbone from 'backbone';
 export { default } from 'backbone';
 import { Events } from 'marionette';
 
-extend(Backbone, Events);
 extend(Backbone.Model.prototype, Events);
 extend(Backbone.Collection.prototype, Events);
 extend(Backbone.View.prototype, Events);
 extend(Backbone.Router.prototype, Events);
-extend(Backbone.History.prototype, Events);
